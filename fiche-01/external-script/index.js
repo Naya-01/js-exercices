27f9744c3350120f0eac39f2f797b0ecd2f7302e
rayan abarkan <rayan.abarkan@student.vinci.be>
2021-09-21 08:30:04 +0200

    exo 2.2 et 2.3

--- fiche-01/external-script/index.js
+++ fiche-01/external-script/index.js
@@ -1,11 +1,11 @@
-function addDateTime(message="Bonjour"){
-    const dateTimeNow = new Date();
-    console.log(dateTimeNow.toLocaleDateString()); // 17/08/2020
-    console.log(dateTimeNow.toLocaleTimeString()); // 13:26:15
-    return message + ", nous sommes le "+dateTimeNow.toLocaleDateString() + " et il est " + dateTimeNow.toLocaleTimeString();
-}
-
-alert(addDateTime());
+// function addDateTime(message="Bonjour"){
+//     const dateTimeNow = new Date();
+//     console.log(dateTimeNow.toLocaleDateString()); // 17/08/2020
+//     console.log(dateTimeNow.toLocaleTimeString()); // 13:26:15
+//     return message + ", nous sommes le "+dateTimeNow.toLocaleDateString() + " et il est " + dateTimeNow.toLocaleTimeString();
+// }
+//
+// alert(addDateTime());
 
 
 const btn = document.getElementById('clique');
@@ -36,7 +36,20 @@ reset.onclick = function (){
 reset.onmouseover = function (){
     nbClique.style.color = 'black';
 }
-
-
-
-
+// exo horloge
+let compteur=0;
+let date=document.getElementById("dateHeure");
+let btnClock=document.getElementById("ClockClick");
+function currentDate(){
+    let dateNow = new Date();
+    date.innerHTML = dateNow.toDateString() + " "+dateNow.getHours()+"h "+dateNow.getMinutes()+'m '+dateNow.getSeconds()+"s";
+}
+let mydate =setInterval(currentDate,1000);
+btnClock.addEventListener('click',()=>{
+    compteur++;
+    if(compteur%2===0){
+        mydate =setInterval(currentDate,500);
+    }else{
+        clearInterval(mydate);
+    }
+})
