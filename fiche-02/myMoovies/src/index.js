928bd0914b772644121669e29f11dae9dba6c98a
rayan abarkan <rayan.abarkan@student.vinci.be>
2021-09-28 09:02:42 +0200

    myMoovies : step 1 : bundler

--- fiche-02/myMoovies/src/index.js
+++ fiche-02/myMoovies/src/index.js
@@ -6,16 +6,3 @@ import './stylesheets/main.css';
 import { AnalogClock } from 'customizable-analog-clock';
 // This is the entry point to your app : add all relevant import and custom code
 
-
-
-const clock = new AnalogClock({
-    htmlElement : 'my-clock',
-    showDate: true,
-    showDigitalClock: true,
-    showIndicators: true,
-    clockIndicators : ['1', '2', '3' , '4', '🐊', '💻', '🐅', '🐼', '🐘', '🚴‍♂️', '🏂', '🧑'],
-    styleOptions : {
-        fontSize: '15px',
-        fontFamily: 'Roboto'
-    }
-});
