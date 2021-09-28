0ca693ece51c6673480ee2d600d8c8e436713c88
rayan abarkan <rayan.abarkan@student.vinci.be>
2021-09-28 09:56:54 +0200

    myMoovies : step 2 : dynamic homepage

--- fiche-02/myMoovies/src/index.js
+++ fiche-02/myMoovies/src/index.js
@@ -2,7 +2,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Custom styles
 import './stylesheets/main.css';
+import logo1 from "./img/426983.jpg";
+import logo2 from "./img/affiche_370x0.jpg";
 
-import { AnalogClock } from 'customizable-analog-clock';
-// This is the entry point to your app : add all relevant import and custom code
 
+
+let page = document.getElementById("page");
+let image1 = document.createElement("img");
+image1.src=logo1;
+let image2 = document.createElement("img");
+image2.src=logo2;
+let txt = document.createElement("p");
+txt.innerText = "Ei Sei le futur roi de chine !!!"
+
+page.appendChild(txt);
+page.appendChild(image1);
+page.appendChild(image2);
