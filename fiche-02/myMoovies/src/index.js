80edc33cfd7fd3fcef9987621bada872025d4ccb
rayan abarkan <rayan.abarkan@student.vinci.be>
2021-09-29 16:57:25 +0200

    ajout du form

--- fiche-02/myMoovies/src/index.js
+++ fiche-02/myMoovies/src/index.js
@@ -2,19 +2,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Custom styles
 import './stylesheets/main.css';
-import logo1 from "./img/426983.jpg";
-import logo2 from "./img/affiche_370x0.jpg";
+// import logo1 from "./img/426983.jpg";
+// import logo2 from "./img/affiche_370x0.jpg";
+// let page = document.getElementById("page");
+// let image1 = document.createElement("img");
+// image1.src=logo1;
+// let image2 = document.createElement("img");
+// image2.src=logo2;
+// let txt = document.createElement("p");
+// txt.innerText = "Ei Sei le futur roi de chine !!!"
+// page.appendChild(txt);
+// page.appendChild(image1);
+// page.appendChild(image2);
 
-
-
-let page = document.getElementById("page");
-let image1 = document.createElement("img");
-image1.src=logo1;
-let image2 = document.createElement("img");
-image2.src=logo2;
-let txt = document.createElement("p");
-txt.innerText = "Ei Sei le futur roi de chine !!!"
-
-page.appendChild(txt);
-page.appendChild(image1);
-page.appendChild(image2);
