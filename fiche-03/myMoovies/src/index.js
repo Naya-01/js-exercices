228702d69f5b9431f5f54258ba3fcef274c8aac8
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2021-10-10 14:54:46 +0200

    myMoovies : step 3 : refactoring with modules & classes. New : add

--- fiche-03/myMoovies/src/index.js
+++ fiche-03/myMoovies/src/index.js
@@ -2,46 +2,59 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Custom styles
 import './stylesheets/main.css';
-
-
-import logo1 from "./img/426983.jpg";
-import logo2 from "./img/affiche_370x0.jpg";
+import Film from "./Domain/Film.js";
+import FilmLibrary from "./Domain/FilmLibrary.js";
 
 "use strict";
 
 let page = document.getElementById("page");
-let image1 = document.createElement("img");
-image1.src=logo1;
-let image2 = document.createElement("img");
-image2.src=logo2;
-let txt = document.createElement("p");
-txt.innerText = "Ei Sei le futur roi de chine !!!"
 let form = `<form id="form">
         <div class="form-group align-content-center">
           <label>Nom du film</label> <br>
-          <input type="text" class="form-control-lg" placeholder="Nom du film" id="film">
+          <input type="text" class="form-control-lg" placeholder="Nom du film" id="film" required>
         </div>
         <div class="form-group">
           <label>durée du film en minutes</label> <br>
-          <input type="text" class="form-control-lg" placeholder="durée du film en minutes" id="duree">
+          <input type="text" class="form-control-lg" placeholder="durée du film en minutes" id="duree" required>
         </div>
         <div class="form-group">
           <label>Budget du film</label> <br>
-          <input type="text" class="form-control-lg" placeholder="Budget" id="budget">
+          <input type="text" class="form-control-lg" placeholder="Budget" id="budget" required>
         </div> 
         <div class="form-group">
           <label>Lien</label> <br>
-          <input type="text" class="form-control-lg" placeholder="lien vers la description" id="lien">
+          <input type="text" class="form-control-lg" placeholder="lien vers la description" id="lien" required>
         </div> <br>
         <button type="submit" class="btn btn-primary" id="btn">Create table</button>
       </form>
 
       </form> <br>`;
-let myDiv = document.createElement('div');
-let divForm = document.createElement('dev');
+let divForm = document.createElement('div');
 divForm.innerHTML=form;
-myDiv.appendChild(txt);
-myDiv.appendChild(image1);
-myDiv.appendChild(image2);
-page.appendChild(myDiv);
 page.appendChild(divForm);
+
+//notification
+let msg = "";
+let notification = document.createElement('div');
+
+//event form
+let formFilm=document.getElementById('form');
+formFilm.addEventListener('submit',addFilmTable);
+
+
+// div to add table
+let divTable= document.createElement("div");
+let arrayFilm = new FilmLibrary();
+function addFilmTable(e){
+    e.preventDefault();
+    let nomFilm=document.getElementById('film').value;
+    let dureeFilm=document.getElementById('duree').value;
+    let budgetFilm=document.getElementById('budget').value;
+    let lienFilm=document.getElementById('lien').value;
+    let film = new Film(nomFilm,dureeFilm,budgetFilm,lienFilm);
+    arrayFilm.addFilm(film);
+    divTable.innerHTML=arrayFilm.getHtmlTable();
+};
+
+
+page.appendChild(divTable);
