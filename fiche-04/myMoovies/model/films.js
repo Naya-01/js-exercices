ad749b204cee6a84e1f0e3229eaf17ea781216a9
rayan abarkan <rayan.abarkan@student.vinci.be>
2021-10-11 18:09:45 +0200

    transfert sur fix

---
+++ fiche-04/myMoovies/model/films.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const defaultFilms = [
+    {
+        id: 1,
+        title: "4 fromages",
+        duration: "150",
+    },
+    {
+        id: 2,
+        title: "Vegan",
+        content: "Tomates, Courgettes, Oignons, Aubergines, Poivrons",
+    },
+    {
+        id: 3,
+        title: "Vegetarian",
+        content: "Mozarella, Tomates, Oignons, Poivrons, Champignons, Olives",
+    },
+    {
+        id: 4,
+        title: "Alpage",
+        content: "Gruyère, Mozarella, Lardons, Tomates",
+    },
+    {
+        id: 5,
+        title: "Diable",
+        content: "Tomates, Mozarella, Chorizo piquant, Jalapenos",
+    },
+];
